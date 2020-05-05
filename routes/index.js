d3a54292972f17b147fd0689ae3b54a7dd766c20
Tayfun Sur <tayfunsur@gmail.com>
2020-05-05 22:19:37 +0300

    express-validator is added and user in the register function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
-const passportConfig = require('../passport');
+// const passportConfig = require('../passport');
 const User = require('../models/user');
 const Post = require('../models/post');
+
+const { body } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -35,43 +37,57 @@ router.post('/post', (req,res,next) => {
       });
   })
 })
-router.post('/register', (req, res, next) => {
-  const { firstName, lastName, username, password } = req.body;
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      res
-        .status(500)
-        .json({ message: { msgBody: 'Error has occured', msgError: true } });
-    }
-    if (user) {
-      res.status(500).json({
-        message: { msgBody: 'Username is already taken', msgError: true },
-      });
-    } else {
-      let newUser = new User({
-        first_name: firstName,
-        last_name: lastName,
-        username: username,
-        password: password,
-      });
-      newUser.save((err) => {
-        if (err) {
-          console.log(newUser);
+router.post('/register', [
+  body('first_name', 'Fields cannot be empty.').trim().isLength({ min: 1 }),
+  body('last_name', 'Fields cannot be empty.').trim().isLength({ min: 1 }),
+  body('username', 'Fields cannot be empty.').trim().isLength({ min: 1 }),
 
-          res.status(500).json({
-            message: { msgBody: 'Could not be saved', msgError: err },
-          });
-        } else
-          res.status(201).json({
-            message: {
-              msgBody: 'Account successfully created',
-              msgError: false,
-            },
-          });
-      });
-    }
-  });
-});
+  body('*').escape(),
+
+
+  (req, res, next) => {
+    const { firstName, lastName, username, password } = req.body;
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ message: { msgBody: 'Error has occured', msgError: true } });
+      }
+      if (user) {
+        res.status(500).json({
+          message: { msgBody: 'Username is already taken', msgError: true },
+        });
+      } else {
+  
+        
+        let newUser = new User({
+          first_name: firstName,
+          last_name: lastName,
+          username: username,
+          password: password,
+        });
+        console.log(newUser)
+        // newUser.save((err) => {
+        //   if (err) {
+        //     console.log(newUser);
+  
+        //     res.status(500).json({
+        //       message: { msgBody: 'Could not be saved', msgError: err },
+        //     });
+        //   } else
+        //     res.status(201).json({
+        //       message: {
+        //         msgBody: 'Account successfully created',
+        //         msgError: false,
+        //       },
+        //     });
+        // });
+      }
+    });
+  }
+])
+  
+
 
 router.get('/login', (req, res, next) => {
   res.render('login', { title: 'Login Page' });
